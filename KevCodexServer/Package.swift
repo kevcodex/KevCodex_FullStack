33bf4d019e47e12de5869dc9886c5a70d4abb76f
Kevin Chen <kchen@phunware.com>
2019-01-22 21:21:34 -0800

    Setup vapor and move perfect into legacy folder

--- KevCodexServer/Package.swift
+++ KevCodexServer/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,7 +16,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "Run", dependencies: ["App"]),
-        .target(name: "App", dependencies: ["PerfectHTTPServer", "PerfectWebRedirects", "MongoKitten", "Vapor"]),
+        .target(name: "App", dependencies: ["MongoKitten", "Vapor"]),
+        .target(name: "LegacyApp", dependencies: ["PerfectHTTPServer", "PerfectWebRedirects", "MongoKitten"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
--- KevCodexServer/Sources/Run/main.swift
+++ KevCodexServer/Sources/Run/main.swift
@@ -1,10 +1,8 @@
 import App
-import PerfectLib
+import Vapor
 
 do {
-    try App.start()
-} catch PerfectError.networkError(let code, let message) {
-    print("NETWORK error \(code), \(message)")
+    try app(.detect()).run()
 } catch {
     print("Failed to start: \(error)")
 }
