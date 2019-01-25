fdb9c820bb777dcd98ba518edf64733bb6a3b501
Kevin Chen <kchen@phunware.com>
2019-01-24 22:26:22 -0800

    Comment out Perfect stuff

--- KevCodexServer/Package.swift
+++ KevCodexServer/Package.swift
@@ -7,10 +7,10 @@ let package = Package(
     name: "KevCodexServer",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+//        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+//        .package(url: "https://github.com/PerfectlySoft/Perfect-WebRedirects", from: "3.0.0"),
         .package(url: "https://github.com/OpenKitten/MongoKitten.git", from: "5.0.0"),
         .package(url: "https://github.com/OpenKitten/MeowVapor.git", from: "2.0.0"),
-        .package(url: "https://github.com/PerfectlySoft/Perfect-WebRedirects", from: "3.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0")
     ],
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "Run", dependencies: ["App"]),
         .target(name: "App", dependencies: ["MongoKitten", "Vapor", "MeowVapor", "Leaf"]),
-        .target(name: "LegacyApp", dependencies: ["PerfectHTTPServer", "PerfectWebRedirects", "MongoKitten"]),
+//        .target(name: "LegacyApp", dependencies: ["PerfectHTTPServer", "PerfectWebRedirects", "MongoKitten"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
