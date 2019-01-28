bb7cbceffa6a7110ee2837f881d815bf9e383344
Kevin Chen <kchen@phunware.com>
2019-01-27 22:47:46 -0800

    Add create and delete hiking endpoints. Parse out short description for display

--- KevCodexServer/Sources/App/Hiking/HikingController.swift
+++ KevCodexServer/Sources/App/Hiking/HikingController.swift
@@ -6,11 +6,14 @@
 //
 
 import Vapor
+import MongoKitten
 
 struct HikingController: RouteCollection {
     func boot(router: Router) throws {
         let apiRouter = router.grouped("api", "hikes")
         apiRouter.get(use: getAllHikes)
+        apiRouter.post(use: addHike)
+        apiRouter.delete("name", String.parameter, use: deleteHike)
     }
     
     func getAllHikes(_ req: Request) throws -> Future<[Hike]> {
@@ -21,5 +24,52 @@ struct HikingController: RouteCollection {
             return context.find(Hike.self).getAllResults()
         }
     }
-
+    
+    func addHike(_ req: Request) throws -> Future<HTTPStatus> {
+        
+        guard let apiKey = req.http.headers["apiKey"].first else {
+            throw Abort(.forbidden, reason: "Missing API Header")
+        }
+        
+        guard apiKey == globalApiKey else {
+            throw Abort(.forbidden, reason: "Invalid API Key")
+        }
+        
+        return try req.content.decode(HikeBody.self).flatMap(to: HTTPStatus.self) { (hikeBody) in
+            
+            let meow = req.meow()
+            
+            let hikeFuture = meow.map { (context) -> Future<Void> in
+                let hike = Hike(hikeBody: hikeBody)
+                
+                return context.save(hike)
+            }
+            
+            return hikeFuture.transform(to: HTTPStatus.created)
+        }
+    }
+    
+    func deleteHike(_ req: Request) throws -> Future<HTTPStatus> {
+        
+        guard let apiKey = req.http.headers["apiKey"].first else {
+            throw Abort(.forbidden, reason: "Missing API Header")
+        }
+        
+        guard apiKey == globalApiKey else {
+            throw Abort(.forbidden, reason: "Invalid API Key")
+        }
+        
+        let name = try req.parameters.next(String.self)
+        
+        let meow = req.meow()
+        
+        let futureInt = meow.flatMap { (context) -> Future<Int> in
+            let namePath = try Hike.makeQueryPath(for: \Hike.title)
+            return context.deleteOne(Hike.self, where: namePath == name)
+        }
+        
+        return futureInt.map { (int) -> (HTTPStatus) in
+            return int == 1 ? .noContent : .notFound
+        }
+    }
 }
--- KevCodexServer/Sources/App/Website/Models/Hike.swift
+++ KevCodexServer/Sources/App/Website/Models/Hike.swift
@@ -8,11 +8,68 @@
 import Vapor
 import MeowVapor
 
+/// Specifically just for decoding body in request
+struct HikeBody: Decodable {
+    let title: String
+    let location: String
+    let distance: Double
+    let hikingTime: Double
+    let elevationGain: Int
+    let difficulty: String?
+    let description: String
+}
+
 final class Hike: Content, QueryableModel {
     var _id: ObjectId
     
     let title: String
+    let location: String
+    let distance: Double
+    let hikingTime: Double
+    let elevationGain: Int
+    let difficulty: String?
     let description: String
+    
+    let shortDescription: String?
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self._id = ObjectId()
+        self.title = try container.decode(String.self, forKey: .title)
+        self.location = try container.decode(String.self, forKey: .title)
+        self.distance = try container.decode(Double.self, forKey: .distance)
+        self.hikingTime = try container.decode(Double.self, forKey: .hikingTime)
+        self.elevationGain = try container.decode(Int.self, forKey: .elevationGain)
+        self.difficulty = try container.decode(String.self, forKey: .difficulty)
+        self.description = try container.decode(String.self, forKey: .description)
+        
+        self.shortDescription = description.components(separatedBy: ".").first
+    }
+    
+    init(title: String,
+         location: String,
+         distance: Double,
+         hikingTime: Double,
+         elevationGain: Int,
+         difficulty: String?,
+         description: String) {
+        
+        self._id = ObjectId()
+        self.title = title
+        self.location = location
+        self.distance = distance
+        self.hikingTime = hikingTime
+        self.elevationGain = elevationGain
+        self.difficulty = difficulty
+        self.description = description
+        
+        self.shortDescription = description.components(separatedBy: ".").first
+    }
+    
+    convenience init(hikeBody: HikeBody) {
+        self.init(title: hikeBody.title, location: hikeBody.location, distance: hikeBody.distance, hikingTime: hikeBody.hikingTime, elevationGain: hikeBody.elevationGain, difficulty: hikeBody.difficulty, description: hikeBody.description)
+    }
     
     static var collectionName: String {
         return "hiking"
