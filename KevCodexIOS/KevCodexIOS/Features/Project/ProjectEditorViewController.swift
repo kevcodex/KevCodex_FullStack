e173b9848614a31b0b3fe3efca31f9e3209d0d6f
Kevin Chen <kevchen2@gmail.com>
2019-02-14 19:30:47 -0800

    Add project editor VC

---
+++ KevCodexIOS/KevCodexIOS/Features/Project/ProjectEditorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectEditorViewController.swift
+//  KevCodexIOS
+//
+//  Created by Kevin Chen on 2/13/19.
+//  Copyright © 2019 Kirby. All rights reserved.
+//
+
+import UIKit
+
+final class ProjectEditorViewController: UIViewController {
+
+
+}
+
+extension ProjectEditorViewController: StoryboardInitializable {
+    static var storyboardName: String {
+        return "Project"
+    }
+}
